5eeb52c3cdc026eb31b13529ca348b7019723dc0
Marko Vidaković <markovidakovic29@gmail.com>
2022-02-21 20:59:39 +0100

    -

--- 02_everyday_types/index.ts
+++ 02_everyday_types/index.ts
@@ -97,3 +97,80 @@ printCoord({ x: 100, y: 100 });
 
 // You can actually use a type alias to give a name to any type at all, not just an object type. For example, a type alias can name a union type:
 type ID = number | string;
+
+// Interfaces
+
+// an interface declaration is another way to name an object type:
+
+interface Point {
+    x: number;
+    y: number;
+}
+
+/**
+ * Type aliases and interfaces are very similar, and in may cases you can choose between them freely.
+ * Almost all features of an interface are available in type,
+ * the key distinction is that a type cannot be re-opened to add new properties
+ * vs an interface which is always extendable
+ */
+
+// extending an interface
+interface Animal {
+    name: string;
+}
+
+interface Bear extends Animal {
+    honey: boolean;
+}
+
+const bear = getBear();
+bear.name
+bear.honey
+
+// extending a type via intersections
+type Animal = {
+    name: string;
+}
+
+type Bear = Animal & {
+    honey: boolean;
+}
+
+const bear = getBear();
+bear.name
+bear.honey
+
+// adding new fields to an existing interface
+interface window {
+    title: string;
+}
+
+interface window {
+    ts: TypeScriptAPI
+}
+
+const src = 'const a = "Hello World"';
+window.statusbar.transpileMOdule(src, {});
+
+// a type cannot be changed after being created
+type Window = {
+    title: string;
+}
+
+type Window = {
+    ts: TypeScriptAPI
+}
+
+// Error: Duplicate identifier 'Window'
+
+// Type assertions
+
+const myCanvas = document.getElementById("main_canvas") as HTMLCanvasElement;
+// you can use the angle-bracket syntax - exept if the code is in a .tsx file
+const myCanvas = <HTMLCanvasElement>document.getElementById("main_canvas");
+
+// non-null assertion operator (Postfix!)
+function liveDangerously(x?: number | null) {
+    // No error
+    console.log(x!.toFixed());
+}
